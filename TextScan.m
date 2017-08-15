fa78420ff6be24aaf357151f24fb492d3ba6aaf5
jordanaron22 <jordanaron22@gmail.com>
2017-08-15 17:16:48 -0400

    updated w/ textscan

--- TextScan.m
+++ TextScan.m
@@ -1,9 +1,12 @@
+#Change 'SmartSeq' to what is being imported in the below line
 fid = fopen('SmartSeq.txt','r')
 tline = fgetl(fid);
 fclose(fid);
 sampleNames = cellstr(strvcat(strsplit(tline,'\t')));
 totSamps = length(sampleNames); 
+#Change 'SmartSeq' to what is being imported in the below line
 fid = fopen('SmartSeq.txt','r');
- C = textscan(fid, [repmat('%f',1,totSamps)], 'Delimiter', '\t', 'HeaderLines',0);
+C = textscan(fid, [repmat('%f',1,totSamps)], 'Delimiter', '\t', 'HeaderLines',0);
 fclose(fid);
+%Change ncounts to outpute name.  i.e. 'OldESGeneNames', 'NewESGeneNames', 'SmartSeqNames', or 'SmartSeq'
 ncounts = [C{1:end}]; 
